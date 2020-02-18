772c82fe4461c68d1fa906d6aca1bf465f9d2af0
Silvian91 <silvian@vivy.com>
2020-02-18 21:03:41 +0100

    [#15] Add ShowWorkoutItemWrapper sealed class and functionality

--- app/src/main/java/com/example/workoutapp/showworkout/adapter/ShowWorkoutItemWrapper.kt
+++ app/src/main/java/com/example/workoutapp/showworkout/adapter/ShowWorkoutItemWrapper.kt
@@ -1,4 +1,14 @@
 package com.example.workoutapp.showworkout.adapter
 
-class ShowWorkoutItemWrapper {
+import com.example.workoutapp.domain.workout.model.WorkoutModel
+
+sealed class ShowWorkoutItemWrapper(
+    val type: ItemType
+) {
+    enum class ItemType {
+        WORKOUT_TITLE
+    }
+
+    data class WorkoutTitle(val workoutTitle: WorkoutModel) :
+        ShowWorkoutItemWrapper(ItemType.WORKOUT_TITLE)
 }
