6f7b6e18016ae04efedd3d95ba72d62a9ffc1f5e
Silvian91 <silvian@vivy.com>
2020-02-18 21:21:46 +0100

    [#17] Add BaseViewHolder class and functionality

--- app/src/main/java/com/example/workoutapp/common/BaseViewHolder.kt
+++ app/src/main/java/com/example/workoutapp/common/BaseViewHolder.kt
@@ -1,4 +1,10 @@
 package com.example.workoutapp.common
 
-class BaseViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder<T> internal constructor(view: View): RecyclerView.ViewHolder(view) {
+
+    abstract fun bind(model: T)
+
 }
