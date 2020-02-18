13853b7a1140155558e93743531480b5a344b3a5
Silvian91 <silvian@vivy.com>
2020-02-18 22:24:50 +0100

    [#15] Add ShowRoutineItemWrapper sealed class and implementation

--- app/src/main/java/com/example/workoutapp/showroutine/adapter/ShowRoutineItemWrapper.kt
+++ app/src/main/java/com/example/workoutapp/showroutine/adapter/ShowRoutineItemWrapper.kt
@@ -1,4 +1,18 @@
 package com.example.workoutapp.showroutine.adapter
 
-class ShowRoutineItemWrapper {
+import com.example.workoutapp.domain.routine.model.RoutineModel
+
+sealed class ShowRoutineItemWrapper(
+    val type: ItemType,
+    var isFirstItem: Boolean = false
+) {
+
+    enum class ItemType {
+        TITLE,
+        ENTRY
+    }
+
+    data class Title(val title: String) : ShowRoutineItemWrapper(ItemType.TITLE)
+
+    data class Entry(val routine: RoutineModel) : ShowRoutineItemWrapper(ItemType.ENTRY)
 }
