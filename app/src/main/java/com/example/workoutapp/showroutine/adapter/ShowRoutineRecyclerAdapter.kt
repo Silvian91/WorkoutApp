e0fa21b540723a33827eb9e2e3e6e2cc858f154a
Silvian91 <silvian@vivy.com>
2020-02-18 22:28:38 +0100

    [#15] Refactor ShowRoutineRecyclerAdapter to use ShowRoutineItemWrapper

--- app/src/main/java/com/example/workoutapp/showroutine/adapter/ShowRoutineRecyclerAdapter.kt
+++ app/src/main/java/com/example/workoutapp/showroutine/adapter/ShowRoutineRecyclerAdapter.kt
@@ -4,32 +4,62 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workoutapp.R
-import com.example.workoutapp.model.routine.RoutineEntity
+import com.example.workoutapp.showroutine.adapter.ShowRoutineItemWrapper.ItemType.TITLE
+import com.example.workoutapp.showroutine.adapter.viewholder.ShowRoutineEntryViewHolder
+import com.example.workoutapp.showroutine.adapter.viewholder.ShowRoutineTitleViewHolder
 
-class ShowRoutineRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class ShowRoutineRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private var routinePairs: List<RoutineEntity> = ArrayList()
+    private var items: List<ShowRoutineItemWrapper> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ShowRoutineViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.layout_routine_list, parent, false)
-        )
-    }
-
-    override fun getItemCount(): Int {
-        return routinePairs.size
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(holder){
-            is ShowRoutineViewHolder -> {
-                holder.bind(routinePairs[position])
+        return when (viewType) {
+            TITLE.ordinal -> {
+                ShowRoutineTitleViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                        R.layout.view_holder_routines_title,
+                        parent,
+                        false
+                    )
+                )
+            }
+            else -> {
+                ShowRoutineEntryViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                        R.layout.view_holder_routines_entry,
+                        parent,
+                        false
+                    )
+                )
             }
         }
     }
 
-    fun setData(routines: List<RoutineEntity>){
-        routinePairs = routines
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ShowRoutineEntryViewHolder -> {
+                val entry = items[position] as ShowRoutineItemWrapper.Entry
+                entry.isFirstItem = position == 0
+                holder.bind(entry)
+            }
+            is ShowRoutineTitleViewHolder -> {
+                val title = items[position] as ShowRoutineItemWrapper.Title
+                title.isFirstItem = position == 0
+                holder.bind(title)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].type.ordinal
+    }
+
+    fun setData(items: List<ShowRoutineItemWrapper>) {
+        this.items = items
         notifyDataSetChanged()
     }
 
