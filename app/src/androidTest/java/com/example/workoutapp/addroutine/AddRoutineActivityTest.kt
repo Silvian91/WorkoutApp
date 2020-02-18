f248061b3346c3d451c308a88a787a99941500fd
Silvian91 <silvian@vivy.com>
2020-02-18 21:10:29 +0100

    [#10] Implement integration tests for onContinueClicked

--- app/src/androidTest/java/com/example/workoutapp/addroutine/AddRoutineActivityTest.kt
+++ app/src/androidTest/java/com/example/workoutapp/addroutine/AddRoutineActivityTest.kt
@@ -1,17 +1,13 @@
 package com.example.workoutapp.addroutine
 
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.workoutapp.R
-import org.hamcrest.CoreMatchers.anything
-import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,7 +28,7 @@ class AddRoutineActivityTest {
     }
 
     @Test
-    fun test_onContinueClickedSuccessful(){
+    fun test_onContinueClickedSuccessful() {
         onView(withId(R.id.routine_name))
             .check(matches(isDisplayed()))
             .perform(typeText("name"))
