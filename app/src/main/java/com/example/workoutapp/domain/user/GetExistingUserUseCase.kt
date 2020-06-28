cc226aaaac58e3531c4e10172f5c47c72979b19e
Silvian91 <silvian@vivy.com>
2020-06-28 20:49:23 +0200

    [#60] Add use case to check if there are existing users

--- app/src/main/java/com/example/workoutapp/domain/user/GetExistingUserUseCase.kt
+++ app/src/main/java/com/example/workoutapp/domain/user/GetExistingUserUseCase.kt
@@ -1,4 +1,18 @@
 package com.example.workoutapp.domain.user
 
-interface CheckIfThereAreUsersUseCase {
+import com.example.workoutapp.domain.common.BaseSingleUseCase
+import com.example.workoutapp.domain.common.BaseUseCase
+import com.example.workoutapp.domain.user.GetExistingUserUseCase.Input
+import com.example.workoutapp.domain.user.GetExistingUserUseCase.Output
+
+interface GetExistingUserUseCase : BaseSingleUseCase<Input, Output> {
+
+    object Input : BaseUseCase.Input
+
+    sealed class Output : BaseUseCase.Output {
+
+        object Success: Output()
+        object NoUsers: Output()
+        object ErrorUnknown: Output()
+    }
 }
--- app/src/main/java/com/example/workoutapp/domain/user/GetExistingUserUseCaseImpl.kt
+++ app/src/main/java/com/example/workoutapp/domain/user/GetExistingUserUseCaseImpl.kt
@@ -1,9 +1,24 @@
 package com.example.workoutapp.domain.user
 
+import com.example.workoutapp.domain.user.GetExistingUserUseCase.Input
+import com.example.workoutapp.domain.user.GetExistingUserUseCase.Output
+import com.example.workoutapp.domain.user.GetExistingUserUseCase.Output.*
 import io.reactivex.Single
 
-class GetExistingUserUseCaseImpl : GetExistingUserUseCase {
-    override fun execute(input: GetExistingUserUseCase.Input): Single<GetExistingUserUseCase.Output> {
-        TODO("Not yet implemented")
+class GetExistingUserUseCaseImpl(
+    private val userRepository: UserRepository
+) : GetExistingUserUseCase {
+    override fun execute(input: Input): Single<Output> {
+        return userRepository.getUser()
+            .flatMapSingle {
+                if (it > 0) {
+                    Single.just(Success as Output)
+                }
+                else
+                    Single.just(NoUsers as Output)
+            }
+            .onErrorReturn {
+                ErrorUnknown
+            }
     }
 }
