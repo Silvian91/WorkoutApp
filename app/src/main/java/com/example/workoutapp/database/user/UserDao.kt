07ab91cb62ed460d4ab2d10c51585f22c39f4dfb
Silvian91 <silvian@vivy.com>
2020-06-28 20:51:56 +0200

    [#60] Add database query to check existing users

--- app/src/main/java/com/example/workoutapp/database/user/UserDao.kt
+++ app/src/main/java/com/example/workoutapp/database/user/UserDao.kt
@@ -16,4 +16,7 @@ interface UserDao {
     @Query("SELECT * FROM users WHERE id = :id LIMIT 1")
     fun getUser(id: Long): UserEntity
 
+    @Query("SELECT COUNT(*) from users")
+    fun getUser(): Long
+
 }
