6cd9cc1a26340d47055f4caa1693a4254f12bdeb
Silvian91 <silvian@vivy.com>
2020-08-02 20:33:50 +0200

    [#65] Add softDeleteWorkoutUseCase

--- app/src/main/java/com/example/workoutapp/domain/showworkout/SoftDeleteWorkoutUseCase.kt
+++ app/src/main/java/com/example/workoutapp/domain/showworkout/SoftDeleteWorkoutUseCase.kt
@@ -1,4 +1,16 @@
 package com.example.workoutapp.domain.showworkout
 
-interface SoftDeleteWorkoutUseCase {
+import com.example.workoutapp.domain.common.BaseSingleUseCase
+import com.example.workoutapp.domain.common.BaseUseCase
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Input
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Output
+
+interface SoftDeleteWorkoutUseCase : BaseSingleUseCase<Input, Output> {
+
+    data class Input(val workoutId: Long) : BaseUseCase.Input
+
+    sealed class Output : BaseUseCase.Output {
+        object Success : Output()
+        object ErrorUnknown : Output()
+    }
 }
--- app/src/main/java/com/example/workoutapp/domain/showworkout/SoftDeleteWorkoutUseCaseImpl.kt
+++ app/src/main/java/com/example/workoutapp/domain/showworkout/SoftDeleteWorkoutUseCaseImpl.kt
@@ -1,9 +1,18 @@
 package com.example.workoutapp.domain.showworkout
 
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Input
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Output
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Output.ErrorUnknown
+import com.example.workoutapp.domain.showworkout.SoftDeleteWorkoutUseCase.Output.Success
+import com.example.workoutapp.domain.workout.WorkoutRepository
 import io.reactivex.Single
 
-class SoftDeleteWorkoutUseCaseImpl : SoftDeleteWorkoutUseCase {
-    override fun execute(input: SoftDeleteWorkoutUseCase.Input): Single<SoftDeleteWorkoutUseCase.Output> {
-        TODO("Not yet implemented")
+class SoftDeleteWorkoutUseCaseImpl(
+    private val workoutRepository: WorkoutRepository
+) : SoftDeleteWorkoutUseCase {
+    override fun execute(input: Input): Single<Output> {
+        return workoutRepository.softDeleteWorkout(input.workoutId)
+            .andThen(Single.just(Success as Output))
+            .onErrorReturn { ErrorUnknown }
     }
 }
