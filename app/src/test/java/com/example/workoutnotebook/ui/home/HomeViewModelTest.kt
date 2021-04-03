ed41b3c30386bdf29b21a5dba52fa5e477afaffd
Silvian91 <silvian@vivy.com>
2021-04-03 21:00:05 +0200

    add home view model unit test

---
+++ app/src/test/java/com/example/workoutnotebook/ui/home/HomeViewModelTest.kt
@@ -0,0 +1,103 @@
+package com.example.workoutnotebook.ui.home
+
+import com.example.workoutnotebook.domain.inspirationalquote.GetQuoteUseCase
+import com.example.workoutnotebook.domain.inspirationalquote.model.QuoteModel
+import com.example.workoutnotebook.domain.location.GetLocationUseCase
+import com.example.workoutnotebook.domain.location.model.LocationModel
+import com.example.workoutnotebook.domain.weather.GetWeatherUseCase
+import com.example.workoutnotebook.domain.weather.model.WeatherModel
+import com.example.workoutnotebook.ui.common.BaseTest
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class HomeViewModelTest : BaseTest() {
+    private lateinit var viewModel: HomeViewModel
+    private val getWeatherUseCase: GetWeatherUseCase = mockk()
+    private val getQuoteUseCase: GetQuoteUseCase = mockk()
+    private val getLocationUseCase: GetLocationUseCase = mockk()
+    private val locationModel = LocationModel(22.22, 33.11)
+    private val weatherModel = WeatherModel(22.5, "Berlin")
+    private val quoteModel = QuoteModel("quote", "author")
+
+    @BeforeEach
+    override fun setUp() {
+        super.setUp()
+
+        viewModel = HomeViewModel(
+                getWeatherUseCase,
+                getQuoteUseCase,
+                getLocationUseCase
+        )
+    }
+
+    @Test
+    fun `get current location returns expected output on success`(){
+        every { getLocationUseCase.execute(GetLocationUseCase.Input) } returns
+                Observable.just(GetLocationUseCase.Output.Success(locationModel))
+
+        viewModel.fetchCurrentLocation()
+
+        getLocationUseCase.execute(GetLocationUseCase.Input).test()
+                .assertValue(GetLocationUseCase.Output.Success(locationModel))
+    }
+
+    @Test
+    fun `get current location returns expected output network error`(){
+        every { getLocationUseCase.execute(GetLocationUseCase.Input) } returns
+                Observable.just(GetLocationUseCase.Output.NetworkError)
+
+        viewModel.fetchCurrentLocation()
+
+        getLocationUseCase.execute(GetLocationUseCase.Input).test()
+                .assertValue(GetLocationUseCase.Output.NetworkError)
+    }
+
+    @Test
+    fun `get weather returns expected output on success`(){
+        every { getWeatherUseCase.execute(GetWeatherUseCase.Input(locationModel)) } returns
+                Single.just(GetWeatherUseCase.Output.Success(weatherModel))
+
+        viewModel.fetchWeather(locationModel)
+
+        getWeatherUseCase.execute(GetWeatherUseCase.Input(locationModel)).test()
+                .assertValue(GetWeatherUseCase.Output.Success(weatherModel))
+    }
+
+    @Test
+    fun `get weather returns expected output network error`(){
+        every { getWeatherUseCase.execute(GetWeatherUseCase.Input(locationModel)) } returns
+                Single.just(GetWeatherUseCase.Output.NetworkError)
+
+        viewModel.fetchWeather(locationModel)
+
+        getWeatherUseCase.execute(GetWeatherUseCase.Input(locationModel)).test()
+                .assertValue(GetWeatherUseCase.Output.NetworkError)
+    }
+
+    @Test
+    fun `get quote returns expected output on success`(){
+        every { getQuoteUseCase.execute(GetQuoteUseCase.Input) } returns
+                Single.just(GetQuoteUseCase.Output.Success(quoteModel))
+
+        viewModel.fetchQuote()
+
+        getQuoteUseCase.execute(GetQuoteUseCase.Input).test()
+                .assertValue(GetQuoteUseCase.Output.Success(quoteModel))
+    }
+
+    @Test
+    fun `get quote returns expected output network error`(){
+        every { getQuoteUseCase.execute(GetQuoteUseCase.Input) } returns
+                Single.just(GetQuoteUseCase.Output.NetworkError)
+
+        viewModel.fetchQuote()
+
+        getQuoteUseCase.execute(GetQuoteUseCase.Input).test()
+                .assertValue(GetQuoteUseCase.Output.NetworkError)
+    }
+
+}
